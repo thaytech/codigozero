4e931f597bbf578d20912de5e3c263b0783105ae
thaytech <thaynad781@gmail.com>
2025-06-28 11:13:57 -0300

    Add files via upload
    FUNÇÃO RANDOM

---
+++ 06.5 funcao._random.py
@@ -0,0 +1,10 @@
+import random
+def sorteio(lista):
+    return random.choice(lista)
+
+print(sorteio(["Luis", "Kauã" , "Ingrid" ]))
+print(sorteio(["Luis", "Kauã" , "Ingrid" ]))
+print(sorteio(["Luis", "Kauã" , "Ingrid" ]))
+print(sorteio(["Luis", "Kauã" , "Ingrid" ]))
+print(sorteio(["Luis", "Kauã" , "Ingrid" ]))
+print(sorteio(["Luis", "Kauã" , "Ingrid" ]))
