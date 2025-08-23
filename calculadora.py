a48771a18b75808f2ce287d38e3261eae5d5b1cb
thaytech <thaynad781@gmail.com>
2025-08-23 11:49:30 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+import pyautogui
+
+pyautogui.hotkey('win', 'r')
+pyautogui.sleep(1)
+
+pyautogui.write('calc', interval=0.5)
+pyautogui.press('enter')
+pyautogui.sleep(3)
+
+
+pyautogui.write('20')
+pyautogui.press('+')
+pyautogui.write('20')
+pyautogui.press('enter')
+
+pyautogui.alert('Operação concluída! Verifique o resultado na calculadora.')
